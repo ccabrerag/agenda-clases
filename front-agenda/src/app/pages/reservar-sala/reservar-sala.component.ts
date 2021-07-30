a47a561b2f5a60dbf6faf28ca656047c2677af89
scruzsalazar <scruzsalazar>
2021-07-30 01:36:17 -0400

    actualizacion

--- front-agenda/src/app/pages/reservar-sala/reservar-sala.component.ts
+++ front-agenda/src/app/pages/reservar-sala/reservar-sala.component.ts
@@ -14,7 +14,13 @@ export class ReservarSalaComponent implements OnInit {
 
   salas = [1, 2, 3, 4, 5, 6];
   hour: any;
-  hours = [{ label: '9:00-10:00', start: 9, end: 10 }];
+  hours = [
+    { label: '9:00-10:00', start: 9, end: 10 },
+    { label: '10:00-11:00', start: 10, end: 11 },
+    { label: '11:00-12:00', start: 11, end: 12 },
+    { label: '12:00-13:00', start: 12, end: 13 },
+    { label: '13:00-14:00', start: 13, end: 14 },
+    { label: '14:00-15:00', start: 14, end: 15 }];
 
   constructor(private reservas: ReservasService, private router: Router) {
     this.fecha = new Date();
